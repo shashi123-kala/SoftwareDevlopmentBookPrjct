63f0b1d7d26e8287c39714e3c4d7d9fe44c568d3
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-11 23:56:11 +0530

    Add:Create an entity to set discount for different quantity of books and initialize the discount globally

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -2,13 +2,28 @@ package com.sdb.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.sdb.entity.SoftwareDevelopmentBook;
+import com.sdb.entity.SoftwareDevelopmentBookSetDiscount;
 
 public class CalculateDiscountTest {
 
 	CalculateDiscount calculateDiscount = new CalculateDiscount();
+
+	@BeforeEach
+	public void setup() {
+		List<SoftwareDevelopmentBookSetDiscount> byDifferentCopiesDiscountList = new ArrayList<>();
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(2, 5));
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(3, 10));
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(4, 20));
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(5, 25));
+	}
 
 	@Test
 	public void initializeToBuyBook() {
