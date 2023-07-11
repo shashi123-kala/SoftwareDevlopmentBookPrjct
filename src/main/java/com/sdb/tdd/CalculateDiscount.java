9edb8498432339f924f8e9947d738c55ea1ba289
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-12 01:14:20 +0530

    Add: Buying 2 different books

--- src/main/java/com/sdb/tdd/CalculateDiscount.java
+++ src/main/java/com/sdb/tdd/CalculateDiscount.java
@@ -1,11 +1,135 @@
 package com.sdb.tdd;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import com.sdb.entity.SoftwareDevelopmentBook;
+import com.sdb.entity.SoftwareDevelopmentBookSet;
+import com.sdb.entity.SoftwareDevelopmentBookSetDiscount;
+import com.sdb.entity.SoftwareDevlopmentBookSetQty;
+
 public class CalculateDiscount {
 
+	List<SoftwareDevelopmentBookSetDiscount> discounts;
+
+	public CalculateDiscount(List<SoftwareDevelopmentBookSetDiscount> discounts) {
+		this.discounts = discounts;
+	}
+
+	private List<SoftwareDevlopmentBookSetQty> sdbQtySet = new ArrayList<>();
+
+	public void Add(SoftwareDevelopmentBook book) {
+		SoftwareDevlopmentBookSetQty existedItem = null;
+		for (SoftwareDevlopmentBookSetQty item : sdbQtySet) {
+			if (item.getBook().equals(book))
+				existedItem = item;
+		}
+		if (existedItem != null)
+			existedItem.changeQuantity(existedItem.getQuantity() + 1);
+		else
+			sdbQtySet.add(new SoftwareDevlopmentBookSetQty(book, 1));
+	}
+
 	public double getTotalPrice() {
-		double priceForOneBook = 50.00;
-		double amtAfterDis = priceForOneBook;
-		return amtAfterDis;
+		double amtAfterDis = 0.00;
+		if (sdbQtySet.size() == 1) {
+			double priceForOneBook = 50.00;
+			amtAfterDis = priceForOneBook;
+			return amtAfterDis;
+		} else {
+			List<SoftwareDevelopmentBookSet> setsOfDifferentBooks = getBestCombinationBooksSets(sdbQtySet);
+			double setPrice = 0.0;
+			for (SoftwareDevelopmentBookSet booksSet : setsOfDifferentBooks) {
+				for (SoftwareDevelopmentBook book : booksSet.getBooks()) {
+					setPrice += book.getPrice();
+				}
+				setPrice = setPrice * (1.0 - (booksSet.getDiscount() / 100.0));
+				amtAfterDis += setPrice;
+				setPrice = 0;
+			}
+			return amtAfterDis;
+		}
+	}
+
+	private List<SoftwareDevelopmentBookSet> getBestCombinationBooksSets(
+			List<SoftwareDevlopmentBookSetQty> bookSetQty) {
+		List<List<SoftwareDevelopmentBookSet>> differentBooksSetsCombinations = new ArrayList<>();
+		for (int i = bookSetQty.size(); i >= 1; i--) {
+			differentBooksSetsCombinations.add(calculateDifferentBooksSetsByMaxSize(bookSetQty, i));
+		}
+		List<SoftwareDevelopmentBookSet> optimizeSetList;
+
+		if (differentBooksSetsCombinations.size() > 1)
+			optimizeSetList = selectBooksSetsWithMaxDiscount(differentBooksSetsCombinations);
+		else
+			optimizeSetList = differentBooksSetsCombinations.get(0);
+		return optimizeSetList;
+
+	}
+
+	private List<SoftwareDevelopmentBookSet> calculateDifferentBooksSetsByMaxSize(
+			List<SoftwareDevlopmentBookSetQty> shoppingCartItems, int maxSizeSet) {
+		List<SoftwareDevlopmentBookSetQty> remainingShoppingCartItems = cloneShoppingCartItems(shoppingCartItems);
+		List<SoftwareDevelopmentBookSet> setsOfDifferentBooks = new ArrayList<>();
+		while (remainingShoppingCartItems.size() > 0) {
+			final SoftwareDevelopmentBookSet oneSetOfDifferentBooks = createNextSet(remainingShoppingCartItems,
+					maxSizeSet);
+			setsOfDifferentBooks.add(oneSetOfDifferentBooks);
+		}
+		return setsOfDifferentBooks;
+	}
+
+	private SoftwareDevelopmentBookSet createNextSet(List<SoftwareDevlopmentBookSetQty> remainingShoppingCartItems,
+			int maxSizeSet) {
+		HashSet<SoftwareDevelopmentBook> books = new HashSet<>();
+		for (SoftwareDevlopmentBookSetQty item : new ArrayList<>(remainingShoppingCartItems)) {
+			books.add(item.getBook());
+			if (item.getQuantity() == 1)
+				remainingShoppingCartItems.remove(item);
+			else
+				item.changeQuantity(item.getQuantity() - 1);
+			if (books.size() == maxSizeSet)
+				break;
+		}
+		SoftwareDevelopmentBookSet booksSet = new SoftwareDevelopmentBookSet(books, getDiscount(books.size()));
+		return booksSet;
+	}
+
+	private List<SoftwareDevelopmentBookSet> selectBooksSetsWithMaxDiscount(
+			List<List<SoftwareDevelopmentBookSet>> booksSetsCombinations) {
+		List<SoftwareDevelopmentBookSet> maxDiscountBooksSets = null;
+		int maxBooksSetsDiscount = 0;
+		int totalBooksSetsDiscount = 0;
+		for (List<SoftwareDevelopmentBookSet> booksSets : booksSetsCombinations) {
+			for (SoftwareDevelopmentBookSet booksSet : booksSets) {
+				totalBooksSetsDiscount += booksSet.getDiscount();
+			}
+			if (maxBooksSetsDiscount < totalBooksSetsDiscount) {
+				maxDiscountBooksSets = booksSets;
+				maxBooksSetsDiscount = totalBooksSetsDiscount;
+			}
+			totalBooksSetsDiscount = 0;
+		}
+		return maxDiscountBooksSets;
+	}
+
+	private List<SoftwareDevlopmentBookSetQty> cloneShoppingCartItems(
+			List<SoftwareDevlopmentBookSetQty> shoppingCartItems) {
+		List<SoftwareDevlopmentBookSetQty> shoppingCartItemsCopy = new ArrayList<>();
+		for (SoftwareDevlopmentBookSetQty item : shoppingCartItems) {
+			shoppingCartItemsCopy.add(new SoftwareDevlopmentBookSetQty(item.getBook(), item.getQuantity()));
+		}
+		return shoppingCartItemsCopy;
+	}
+
+	private int getDiscount(int differentBooksCount) {
+		int defaultDiscount = 0;
+		for (SoftwareDevelopmentBookSetDiscount discount : discounts) {
+			if (differentBooksCount == discount.getDifferentCopies())
+				return discount.getDiscount();
+		}
+		return defaultDiscount;
 	}
 
 }
--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -14,7 +14,7 @@ import com.sdb.entity.SoftwareDevelopmentBookSetDiscount;
 
 public class CalculateDiscountTest {
 
-	CalculateDiscount calculateDiscount = new CalculateDiscount();
+	CalculateDiscount calculateDiscount;
 
 	@BeforeEach
 	public void setup() {
@@ -23,6 +23,7 @@ public class CalculateDiscountTest {
 		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(3, 10));
 		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(4, 20));
 		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(5, 25));
+		calculateDiscount = new CalculateDiscount(byDifferentCopiesDiscountList);
 	}
 
 	@Test
@@ -33,7 +34,17 @@ public class CalculateDiscountTest {
 	@Test
 	public void buyingOneBook() {
 		SoftwareDevelopmentBook sdbFirstI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIBook();
+		calculateDiscount.Add(sdbFirstI);
 		assertEquals(50.0, calculateDiscount.getTotalPrice());
+	}
+
+	@Test
+	public void buyingTwoCopiesOfDifferentBook() {
+		SoftwareDevelopmentBook sdbFirstI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIBook();
+		SoftwareDevelopmentBook sdbSecondI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIIBook();
+		calculateDiscount.Add(sdbFirstI);
+		calculateDiscount.Add(sdbSecondI);
+		assertEquals(95.0, calculateDiscount.getTotalPrice());
 	}
 
 }
