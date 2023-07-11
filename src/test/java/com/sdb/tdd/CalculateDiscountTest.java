c2301d41a1507ae89e6785d98092132ae2e8b720
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-12 01:56:18 +0530

    Add:Buying 3 different books

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -54,5 +54,17 @@ public class CalculateDiscountTest {
 		calculateDiscount.Add(sdbSecondI);
 		assertEquals(95.0, calculateDiscount.getTotalPrice());
 	}
+	
+	@Test
+	public void buyingThreeCopiesOfDifferentBook() {
+		SoftwareDevelopmentBook sdbFirstI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIBook();
+		SoftwareDevelopmentBook sdbSecondI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIIBook();
+		SoftwareDevelopmentBook sdbThirdI = SoftwareDevlopmentBookList.GivenASoftwareDevelopmentIIIBook();
+		calculateDiscount.Add(sdbFirstI);
+		calculateDiscount.Add(sdbSecondI);
+		calculateDiscount.Add(sdbThirdI);
+		assertEquals(135, calculateDiscount.getTotalPrice());
+
+	}
 
 }
